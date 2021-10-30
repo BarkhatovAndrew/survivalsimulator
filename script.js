c089b9c880052cf4090425b5880cbbdc7179cdd9
Andrew <barkhatovandrew@gmail.com>
2021-10-30 18:36:58 +0300

    Add more random events

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 // Initialization
 
-const dayCounter = document.querySelector(".day-counter-area");
-const statusArea = document.querySelector(".status-area");
-const btnArea = document.querySelector(".btn-area");
-const startGameBtn = document.querySelector(".start-game-btn");
-const nameInput = document.querySelector(".gamer-name");
-const gameRules = document.querySelector(".rules");
+const dayCounter = document.querySelector('.day-counter-area');
+const statusArea = document.querySelector('.status-area');
+const btnArea = document.querySelector('.btn-area');
+const startGameBtn = document.querySelector('.start-game-btn');
+const nameInput = document.querySelector('.gamer-name');
+const gameRules = document.querySelector('.rules');
 
-let healthBar = document.createElement("p");
-let moneyBar = document.createElement("p");
-let fatigueBar = document.createElement("p");
+let healthBar = document.createElement('p');
+let moneyBar = document.createElement('p');
+let fatigueBar = document.createElement('p');
 
 let waitOneTurn = 0;
 
-nameInput.innerHTML = "";
+nameInput.innerHTML = '';
 
-startGameBtn.addEventListener("click", firstStep);
+startGameBtn.addEventListener('click', firstStep);
 
 function firstStep() {
   const name = nameInput.value;
-  startGameBtn.innerHTML = "Next";
+  startGameBtn.innerHTML = 'Next';
 
-  gameRules.innerHTML = `<p>Hello, <span style="color: #04e600">${name}</span>!</p><p>You have 3 indicators: <span style="color: red">health</span>, <span style="color: #04e600">money</span> and <span style="color: blue">fatigue</span>.</p><p>Fatigue and health have a scale from 0 to 100. The amount of money is unlimited.</p><p>If one of the indicators reaches 0, you will lose.</p>`;
-  nameInput.style.display = "none";
+  gameRules.innerHTML = `<p>Hello, <span class='green'>${name}</span>!</p><p>You have 3 indicators: <span style="color: red">health</span>, <span class='green'>money</span> and <span style="color: blue">fatigue</span>.</p><p>Fatigue and health have a scale from 0 to 100. The amount of money is unlimited.</p><p>If one of the indicators reaches 0, you will lose.</p>`;
+  nameInput.style.display = 'none';
 
-  startGameBtn.addEventListener("click", secondStep);
+  startGameBtn.addEventListener('click', secondStep);
 }
 
 function secondStep() {
@@ -34,28 +34,28 @@ function secondStep() {
 <p>Sometimes it can happen random events that will affect you.</p>
 `;
 
-  startGameBtn.addEventListener("click", thirdStep);
+  startGameBtn.addEventListener('click', thirdStep);
 }
 
 function thirdStep() {
-  startGameBtn.innerHTML = "Start";
+  startGameBtn.innerHTML = 'Start';
   gameRules.innerHTML = `<p>Your goal is to earn 1000 $</p>
 <p>Now you can start the game. Press Start to start :)</p>`;
 
-  startGameBtn.addEventListener("click", startGame);
+  startGameBtn.addEventListener('click', startGame);
 }
 
 // Start Game
 
 function startGame() {
   gameRules.innerHTML = `<p>Choose what you want to do: beg, sleep, eat, shop or status</p>`;
-  startGameBtn.style.display = "none";
-  btnArea.style.display = "unset";
-  dayCounter.style.display = "unset";
+  startGameBtn.style.display = 'none';
+  btnArea.style.display = 'unset';
+  dayCounter.style.display = 'unset';
   dayCounter.innerHTML = `<p>Day ${dayCount}</p>`;
 
   healthBar.innerHTML = `<span style="color: red">Health</span>: ${health}`;
-  moneyBar.innerHTML = `<span style="color: #04e600">Money</span>: ${money}`;
+  moneyBar.innerHTML = `<span class='green'>Money</span>: ${money}`;
   fatigueBar.innerHTML = `<span style="color: blue">Fatigue</span>: ${fatigue}`;
 
   statusArea.append(healthBar);
@@ -87,9 +87,9 @@ function isMaximumPoints() {
 
 function loseCheck() {
   if (health <= 0 || money <= 0 || fatigue <= 0) {
-    btnArea.style.display = "none";
-    statusArea.style.display = "none";
-    dayCounter.style.display = "none";
+    btnArea.style.display = 'none';
+    statusArea.style.display = 'none';
+    dayCounter.style.display = 'none';
 
     gameRules.innerHTML = `<p>You <span style="color: yellow">lose</span>.</p><p>You were able to survive for <span style="color: green">${dayCount}</span> days</p><p>During this time you have earned <span style="color: green">${totalMoney}$</span>. Not bad.</p>`;
   }
@@ -99,9 +99,9 @@ function loseCheck() {
 
 function winCheck() {
   if (money >= 1000) {
-    btnArea.style.display = "none";
-    statusArea.style.display = "none";
-    dayCounter.style.display = "none";
+    btnArea.style.display = 'none';
+    statusArea.style.display = 'none';
+    dayCounter.style.display = 'none';
 
     gameRules.innerHTML = `<p>You <span style="color: yellow">win</span>!</p><p>You were able to survive for <span style="color: green">${dayCount}</span> days</p><p>During this time you have earned <span style="color: green">${totalMoney}$</span>. Not bad.</p>`;
   }
@@ -111,13 +111,13 @@ function winCheck() {
 
 function refreshStatusBars() {
   healthBar.innerHTML = `<span style="color: red">Health</span>: ${health}`;
-  moneyBar.innerHTML = `<span style="color: #04e600">Money</span>: ${money}`;
+  moneyBar.innerHTML = `<span class='green'>Money</span>: ${money}`;
   fatigueBar.innerHTML = `<span style="color: blue">Fatigue</span>: ${fatigue}`;
   dayCounter.innerHTML = `<p>Day ${dayCount}</p>`;
 
   if (
-    gameRules.innerHTML.includes(`<p>Wow! It's a nice day today!`) &&
-    waitOneTurn === 0
+    (gameRules.innerHTML.includes(`<p>Wow!`) && waitOneTurn === 0) ||
+    (gameRules.innerHTML.includes(`<p>Oh shit!`) && waitOneTurn === 0)
   ) {
     waitOneTurn += 1;
   } else if (waitOneTurn === 1) {
@@ -141,7 +141,7 @@ function begging() {
     fatigue -= 5;
     totalMoney += extraMoney;
     money += extraMoney;
-    gameRules.innerHTML += `<p>Wow! It's a nice day today! You earned an extra <span style="color: #04e600">${extraMoney}</span>$</p>`;
+    gameRules.innerHTML += `<p>Wow! It's a nice day today! You earned an extra <span class='green'>${extraMoney}</span>$</p>`;
   }
   regularThings();
 }
@@ -150,6 +150,23 @@ function sleeping() {
   health -= 5;
   money -= 5;
   fatigue += 15;
+
+  let randomChance = Math.floor(Math.random() * 16);
+  switch (randomChance) {
+    case 1:
+      gameRules.innerHTML += `<p>Oh shit! Teenagers stole <span class='green'>15</span>$ from you</p>`;
+      money -= 15;
+      break;
+    case 2:
+      gameRules.innerHTML += `<p>Oh shit! You were attacked by teenagers while you were sleeping. <span class='red'>-15</span> health</p>`;
+      health -= 15;
+      break;
+    case 3:
+      gameRules.innerHTML += `<p>Oh shit! The police woke you up and chased you away. You <span class='blue'>didn't get enough sleep</span>.</p>`;
+      fatigue -= 15;
+      break;
+  }
+
   regularThings();
 }
 
@@ -157,6 +174,7 @@ function eating() {
   health += 15;
   money -= 15;
   fatigue -= 5;
+
   regularThings();
 }
 
@@ -172,10 +190,10 @@ function regularThings() {
 
 // Turn Buttons
 
-const begButton = document.querySelector(".begging");
-const sleepButton = document.querySelector(".sleeping");
-const eatButton = document.querySelector(".eating");
+const begButton = document.querySelector('.begging');
+const sleepButton = document.querySelector('.sleeping');
+const eatButton = document.querySelector('.eating');
 
-begButton.addEventListener("click", begging);
-sleepButton.addEventListener("click", sleeping);
-eatButton.addEventListener("click", eating);
+begButton.addEventListener('click', begging);
+sleepButton.addEventListener('click', sleeping);
+eatButton.addEventListener('click', eating);
